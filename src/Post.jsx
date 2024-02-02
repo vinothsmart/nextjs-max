9bd0045a25ce725943c653465d7cd9307f58c188
vinothsmart <s.vinothkanna90@gmail.com>
2024-02-02 16:57:11 +0530

    post up

--- src/Post.jsx
+++ src/Post.jsx
@@ -1,6 +1,10 @@
+const names = ["vinothkanna", "samarjithraja"];
+
 const Post = () => {
+  const chosenName = Math.random() > 0.5 ? names[0] : names[1];
   return (
     <>
+      <h1>Hi {chosenName}</h1>
       <h1>React is Awesome</h1>
       <h2>It is a JavaScript library</h2>
     </>

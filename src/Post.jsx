129394181f32acf51f35dcc3ac708772148633ea
vinothsmart <s.vinothkanna90@gmail.com>
2024-02-22 13:47:14 +0530

    Refactor Post component to use CSS modules

--- src/Post.jsx
+++ src/Post.jsx
@@ -1,10 +1,11 @@
 import PropTypes from "prop-types";
+import classes from "./Post.module.css";
 
 const Post = ({ author, text }) => {
   return (
-    <div className="post">
-      <p>{author}</p>
-      <p>{text}</p>
+    <div className={classes.post}>
+      <p className={classes.author}>{author}</p>
+      <p className={classes.text}>{text}</p>
     </div>
   );
 };

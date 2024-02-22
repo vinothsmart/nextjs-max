95ecd32e81972fbe6a02f8ff3da9c0a984e10200
vinothsmart <s.vinothkanna90@gmail.com>
2024-02-22 15:36:28 +0530

    Add NewPost component to PostList

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,12 +1,16 @@
+import NewPost from "./NewPost";
 import Post from "./Post";
 import classes from "./PostList.module.css";
 
 const PostList = () => {
   return (
-    <ul className={classes.posts}>
-      <Post author="vinoth" text="React is Awesome" />
-      <Post author="samar" text="Learn React Course" />
-    </ul>
+    <>
+      <NewPost />
+      <ul className={classes.posts}>
+        <Post author="vinoth" text="React is Awesome" />
+        <Post author="samar" text="Learn React Course" />
+      </ul>
+    </>
   );
 };
 

9dc3aedf7cdd07e539d43d921dfe6089418674d4
vinothsmart <s.vinothkanna90@gmail.com>
2024-02-22 17:15:33 +0530

    Refactor NewPost component to use PropTypes

--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -1,23 +1,24 @@
-import { useCallback } from "react";
+import PropTypes from "prop-types";
 import classes from "./NewPost.module.css";
 
-function NewPost() {
-  const handleChange = useCallback((e) => {
-    console.log(e.target.value);
-  }, []);
-
+function NewPost({ handleTextChange, handleAuthorChange }) {
   return (
     <form className={classes.form}>
       <p>
         <label htmlFor="body">Text</label>
-        <textarea id="body" required rows={3} onChange={handleChange} />
+        <textarea id="body" required rows={3} onChange={handleTextChange} />
       </p>
       <p>
         <label htmlFor="name">Your name</label>
-        <input type="text" id="name" required />
+        <input type="text" id="name" required onChange={handleAuthorChange} />
       </p>
     </form>
   );
 }
+
+NewPost.propTypes = {
+  handleTextChange: PropTypes.func.isRequired,
+  handleAuthorChange: PropTypes.func.isRequired,
+};
 
 export default NewPost;
--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,11 +1,26 @@
+import { useCallback, useState } from "react";
 import NewPost from "./NewPost";
 import Post from "./Post";
 import classes from "./PostList.module.css";
 
 const PostList = () => {
+  const [text, setText] = useState("");
+  const [author, setAuthor] = useState("");
+
+  const handleTextChange = useCallback((e) => {
+    setText(e.target.value);
+  }, []);
+
+  const handleAuthorChange = useCallback((e) => {
+    setAuthor(e.target.value);
+  }, []);
+
   return (
     <>
-      <NewPost />
+      <NewPost
+        handleTextChange={handleTextChange}
+        handleAuthorChange={handleAuthorChange}
+      />
       <ul className={classes.posts}>
         <Post author="vinoth" text="React is Awesome" />
         <Post author="samar" text="Learn React Course" />
